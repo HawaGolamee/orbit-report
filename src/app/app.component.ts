281fe0c702d2d40a7a1028903a2f1c5acd1ece60
HawaGolamee <73973675+HawaGolamee@users.noreply.github.com>
2020-12-14 00:04:47 -0600

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'orbit-report';
-}
+  import { Component } from '@angular/core';
+  import { Satallite } from './satallite';
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent {
+    title = 'orbit-report';
+  }
+  let  sourceList: Satallite[]; [
+    constructor() {
+      this.sourceList = [
+        new Satallite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+        new Satallite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+        new Satallite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+        new Satallite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+        new Satallite("ISS", "Space Station", "1998-11-20", "LOW", true),
+      ]
+  }
+  ]
