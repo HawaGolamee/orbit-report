aa930d0f38a56b948b1ea690207c1f0e8b6802f7
HawaGolamee <73973675+HawaGolamee@users.noreply.github.com>
2020-12-18 14:25:53 -0600

    commited

--- src/app/orbit-list/orbit-list.component.spec.ts
+++ src/app/orbit-list/orbit-list.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+  import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { OrbitListComponent } from './orbit-list.component';
+  import { OrbitListComponent } from './orbit-list.component';
 
-describe('OrbitListComponent', () => {
-  let component: OrbitListComponent;
-  let fixture: ComponentFixture<OrbitListComponent>;
+  describe('OrbitListComponent', () => {
+    let component: OrbitListComponent;
+    let fixture: ComponentFixture<OrbitListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrbitListComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ OrbitListComponent ]
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrbitListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(() => {
+      fixture = TestBed.createComponent(OrbitListComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
